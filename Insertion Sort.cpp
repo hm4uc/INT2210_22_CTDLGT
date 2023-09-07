f1fe037d2ae94809dd3e9b61514c6171e1d33e8d
Duesc <124912657+hm4uc@users.noreply.github.com>
2023-09-07 15:27:02 +0700

    Add files via upload

---
+++ Insertion Sort.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int a[1000];
+	int i;
+	for (i = 0; i < n; i++) cin >> a[i];
+	
+	//Insertion Sort
+	for (i = 1; i < n; i++)
+	{
+		int x = a[i];
+		int pos = i;
+		while (pos > 0 && x < a[pos - 1])
+		{
+			a[pos] = a[pos - 1];//Lui so
+			pos--;
+		}
+		a[pos] = x;//Chen so
+	}
+	for (i = 0; i < n; i++) cout << a[i] << " ";
+}
---
+++ Selection Sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int FindMin(int n, int a[], int& dau, int& cuoi)
+{
+	int minn = a[dau], pos = dau;
+	for (int i = dau + 1; i < cuoi; i++)
+	{
+		if (minn > a[i])
+		{
+			minn = a[i];
+			pos = i;
+		}
+	}
+	return pos;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int a[1000];
+	for (int i = 0; i < n; i++) cin >> a[i];
+	int i = 0;
+	while (i < n)
+	{
+		int pos = FindMin(n, a, i, n);
+		swap(a[i], a[pos]);
+		i++;
+	}
+	for (i = 0; i < n; i++) cout << a[i] << " ";
+}
